92dbf0df2cfe63be562285b57fa06fb80c3dd660
Casey Wardle <chwardle@utah.gov>
2023-08-10 16:14:52 -0600

    fix: modify package import

--- src/functions/https/api.f.mjs
+++
@@ -1,8 +0,0 @@
-import { https } from 'firebase-functions/v1';
-import gqlServer from './graphql/server.mjs';
-
-const server = gqlServer();
-
-const api = https.onRequest(server);
-
-export default api;
