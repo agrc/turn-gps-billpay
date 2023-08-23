aac97170c1da7f67627414da5c14c62b746e5ad7
Casey Wardle <chwardle@utah.gov>
2023-08-23 08:20:13 -0600

    fix: graphql config

--- src/functions/https/graphql/server.mjs
+++ src/functions/https/graphql/server.mjs
@@ -1,38 +1,18 @@
 import express from 'express';
 import {ApolloServer} from 'apollo-server-express';
-import {ErrorReporting} from '@google-cloud/error-reporting';
 import 'dotenv/config'
 
 import schema from './schema.mjs';
 import resolvers from './resolvers.mjs';
 
-async function gqlServer() {
-  const errors = new ErrorReporting();
-  const app = express();
+export const apolloServer = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  // Enable graphiql gui
+  introspection: true,
+  playground: true
+});
 
-  const apolloServer = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
-    // Enable graphiql gui
-    introspection: true,
-    playground: true
-  });
+apolloServer.start().then(() => apolloServer.applyMiddleware({app: expressServer, path: '/', cors: true}));
 
-  await apolloServer.start();
-
-  apolloServer.applyMiddleware({app, path: '/', cors: true});
-
-  // Note that express error handling middleware should be attached after all
-  // the other routes and use() calls. See the Express.js docs.
-  app.use(errors.express);
-
-  app.listen(process.env.GRAPHQL_PORT);
-
-  if (process.env.LOCAL) {
-    console.log('🚀🙂😀😃 Server is running on:' + ' ' + `http://localhost:${process.env.GRAPHQL_PORT}`);
-  }
-
-  return app;
-}
-
-export default gqlServer;
+export const expressServer = express();
