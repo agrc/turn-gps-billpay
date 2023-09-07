adfc3f6fc9a02c1cf47128ba87b1f54129550ae1
Casey Wardle <chwardle@utah.gov>
2023-09-07 08:05:05 -0600

    fix: Add codebase default

--- scripts/cleaninstall_win.ps1
+++ scripts/cleaninstall_win.ps1
@@ -11,3 +11,19 @@ if (Test-Path ./node_modules) {
 "Begin npm install"
 npm install
 "End npm install"
+
+if (Test-Path ../function/package-lock.json) {
+    Remove-Item ../function/package-lock.json -Force -ErrorAction Ignore
+    "Removing ../function/package-lock.json"
+}
+
+if (Test-Path ../function/node_modules) {
+    Remove-Item ../function/node_modules -Recurse -Force -ErrorAction Ignore
+    "Removing ../function/node_modules"
+}
+
+cd ../function
+"cd ../function"
+"Begin npm install"
+npm install
+"End npm install"
