ad87b9bbbec572f5029e1bb03a142066c551d46a
Casey Wardle <chwardle@utah.gov>
2023-11-30 08:18:24 -0700

    feat: add spinner while loading subscriptions

--- src/components/pageElements/SubscriptionTable.jsx
+++ src/components/pageElements/SubscriptionTable.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import {
   Icons,
+  Spinner,
   Switch,
   Table,
   TableBody,
@@ -30,82 +32,92 @@ const propTypes = {
     })
 ),
   setTableData: PropTypes.func,
+  lookupStatus: PropTypes.string,
   type: PropTypes.string.isRequired,
 };
 const defaultProps = {};
-function SubscriptionTable({ tableData, setTableData, type }) {
+function SubscriptionTable({
+ tableData, setTableData, lookupStatus, type,
+}) {
   useEffect(() => {
     setTableData(tableData);
   }, [tableData]);
 
-  return (
-    tableData?.length
-      ? (
-        <TableWrapper>
-          <Table className="table table--lines-x table--alt table--v-align-center">
-            <TableHead>
-              <TableSortingRules defaultValue="loginName">
-                <TableSortingRule a11yLabel="Login Name" recordFieldPath="loginName" />
-                <TableSortingRule a11yLabel="Email" recordFieldPath="email" />
-                <TableSortingRule a11yLabel="Additional Email" recordFieldPath="additionalEmail" />
-                <TableSortingRule a11yLabel="Order Number" recordFieldPath="orderNumber" />
-                <TableSortingRule a11yLabel="Effective Date" recordFieldPath="effectiveDate" fieldType={tableSortingRuleFieldType.DATE} />
-                <TableSortingRule a11yLabel="Expiration Date" recordFieldPath="expirationDate" fieldType={tableSortingRuleFieldType.DATE} />
-              </TableSortingRules>
-              <TableHeadRow>
-                <TableHeadCell recordFieldPath="loginName">Login Name</TableHeadCell>
-                <TableHeadCell recordFieldPath="email">Email</TableHeadCell>
-                <TableHeadCell recordFieldPath="additionalEmail">Additional Email</TableHeadCell>
-                <TableHeadCell recordFieldPath="orderNumber">Order Number</TableHeadCell>
-                <TableHeadCell recordFieldPath="effectiveDate">Effective Date</TableHeadCell>
-                <TableHeadCell recordFieldPath="expirationDate">Expiration Date</TableHeadCell>
-                { type === 'pending' ? <TableHeadCell recordFieldPath="activated">Action</TableHeadCell> : ''}
-              </TableHeadRow>
-            </TableHead>
-            <TableBody>
-              <TableBodyData records={tableData} recordIdField="subscriptionId">
-                <TableBodyDataRowTemplate>
-                  <TableBodyDataCellTemplate recordFieldPath="loginName" />
-                  <TableBodyDataCellTemplate recordFieldPath="email" />
-                  <TableBodyDataCellTemplate recordFieldPath="additionalEmail" />
-                  <TableBodyDataCellTemplate recordFieldPath="orderNumber" />
-                  <TableBodyDataCellTemplate recordFieldPath="effectiveDate" />
-                  <TableBodyDataCellTemplate recordFieldPath="expirationDate" />
-                  { type === 'pending'
-                    ? (
-                      <TableBodyDataCellTemplate>
-                        {
-                    ({ record }) => (
-                      <Switch
-                        id={`renew-switch--${record.subscriptionId}`}
-                        label="Renew"
-                        value={record.activated}
-                        onChange={(e) => {
-                          e.stopPropagation();
-                          setTableData(tableData.map((item) => {
-                            if (item.subscriptionId === record.subscriptionId) {
-                              return { ...item, activated: !record.activated };
-                            }
-                            return item;
-                          }));
-                        }}
-                        size="medium"
-                        width={25}
-                        sliderChildren={Icons.IconCheck()}
-                      />
-                    )
-                  }
-                      </TableBodyDataCellTemplate>
-                  )
-                    : ''}
-                </TableBodyDataRowTemplate>
-              </TableBodyData>
-            </TableBody>
-          </Table>
-        </TableWrapper>
-      )
-      : <p>No {type} subscriptions...</p>
-  );
+  const mainTable = () => (
+    <TableWrapper>
+      <Table className="table table--lines-x table--alt table--v-align-center">
+        <TableHead>
+          <TableSortingRules defaultValue="loginName">
+            <TableSortingRule a11yLabel="Login Name" recordFieldPath="loginName" />
+            <TableSortingRule a11yLabel="Email" recordFieldPath="email" />
+            <TableSortingRule a11yLabel="Additional Email" recordFieldPath="additionalEmail" />
+            <TableSortingRule a11yLabel="Order Number" recordFieldPath="orderNumber" />
+            <TableSortingRule a11yLabel="Effective Date" recordFieldPath="effectiveDate" fieldType={tableSortingRuleFieldType.DATE} />
+            <TableSortingRule a11yLabel="Expiration Date" recordFieldPath="expirationDate" fieldType={tableSortingRuleFieldType.DATE} />
+          </TableSortingRules>
+          <TableHeadRow>
+            <TableHeadCell recordFieldPath="loginName">Login Name</TableHeadCell>
+            <TableHeadCell recordFieldPath="email">Email</TableHeadCell>
+            <TableHeadCell recordFieldPath="additionalEmail">Additional Email</TableHeadCell>
+            <TableHeadCell recordFieldPath="orderNumber">Order Number</TableHeadCell>
+            <TableHeadCell recordFieldPath="effectiveDate">Effective Date</TableHeadCell>
+            <TableHeadCell recordFieldPath="expirationDate">Expiration Date</TableHeadCell>
+            {type === 'pending' ? <TableHeadCell recordFieldPath="activated">Action</TableHeadCell> : ''}
+          </TableHeadRow>
+        </TableHead>
+        <TableBody>
+          <TableBodyData records={tableData} recordIdField="subscriptionId">
+            <TableBodyDataRowTemplate>
+              <TableBodyDataCellTemplate recordFieldPath="loginName" />
+              <TableBodyDataCellTemplate recordFieldPath="email" />
+              <TableBodyDataCellTemplate recordFieldPath="additionalEmail" />
+              <TableBodyDataCellTemplate recordFieldPath="orderNumber" />
+              <TableBodyDataCellTemplate recordFieldPath="effectiveDate" />
+              <TableBodyDataCellTemplate recordFieldPath="expirationDate" />
+              {type === 'pending'
+                ? (
+                  <TableBodyDataCellTemplate>
+                    {
+                      ({ record }) => (
+                        <Switch
+                          id={`renew-switch--${record.subscriptionId}`}
+                          label="Renew"
+                          value={record.activated}
+                          onChange={(e) => {
+                            e.stopPropagation();
+                            setTableData(tableData.map((item) => {
+                              if (item.subscriptionId === record.subscriptionId) {
+                                return { ...item, activated: !record.activated };
+                              }
+                              return item;
+                            }));
+                          }}
+                          size="medium"
+                          width={25}
+                          sliderChildren={Icons.IconCheck()}
+                        />
+                      )
+                    }
+                  </TableBodyDataCellTemplate>
+                )
+                : ''}
+            </TableBodyDataRowTemplate>
+          </TableBodyData>
+        </TableBody>
+      </Table>
+    </TableWrapper>
+);
+
+    switch (lookupStatus) {
+      case 'fetching': return <Spinner />;
+      default: return (
+        tableData?.length
+          ? (
+            mainTable()
+          )
+          : <p>No {type} subscriptions...</p>
+      );
+    }
 }
 
 SubscriptionTable.propTypes = propTypes;
