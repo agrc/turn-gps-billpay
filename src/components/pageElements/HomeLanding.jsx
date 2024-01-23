024e70cae51f86a3773e274d6ef34b170afdd772
Casey Wardle <chwardle@utah.gov>
2024-01-23 08:51:29 -0700

    feat: add new banner and css

--- src/components/pageElements/HomeLanding.jsx
+++ src/components/pageElements/HomeLanding.jsx
@@ -22,7 +22,10 @@ function HomeLanding() {
   return (
     <div className="landing-page-template">
       <div className="home-banner">
-        <div className="home-banner__title">TURN<br />GPS</div>
+        <div className="banner-layer-right" />
+        <div className="banner-layer-left">
+          <div className="home-banner__title">TURN<br />GPS</div>
+        </div>
       </div>
       <div className="content-width mb-spacing-xl">
         <h1 className="my-spacing-l text-center">Welcome to Turn GPS</h1>
@@ -73,12 +76,34 @@ function HomeLanding() {
             isOpen
           >
             <ol>
-              <li>The Utah Reference Network GPS (TURN GPS) is a cooperative effort involving many partners with the Utah Geospatial Resource Center (UGRC). This collaborative partnership has made it possible to deliver services at a relatively low cost to users. The partners and the UGRC are committed to delivery of reliable services through TURN GPS. However, some GPS reference stations are located in partnering agencies facilities that may only be accessible during regular business hours. The user understands and acknowledges that this will limit UGRC&apos;s ability to immediately address problems at these facilities outside of regular business hours. Users of this service understand and acknowledge that support for connecting devices will not be provided by the UGRC. Users should seek this support from their local vendors.</li>
-              <li>Users of this service are responsible for verifying the accuracy of the data logged by their device for their own work. This website service is an information tool and users must exercise due diligence to verify all information obtained from this site through standard surveying and geospatial practices and procedures.</li>
-              <li>The corrected GPS data are provided in good faith by UGRC and its partners for its information value. Given the inherent hazards of electronic communication, users acknowledge and understand that there may be omissions or inaccuracies in information contained in this website. As a result, it should not be used as a substitute for consultation with professional and competent advisors. Before making any decision or taking any action based on the information contained in this site, you should consult a professional who is competent in providing professional advice in your area of interest.</li>
-              <li>While the UGRC and its partners have made considerable effort to ensure that the information through this service is accurate, users agree that they will not hold the UGRC and its partners responsible for any errors or omissions, or for the results obtained from the use of this information. All information contained in this website is provided &quot;as is&quot;, with no guarantee of completeness, accuracy, timeliness or the results obtained from the use of the information, and without warranty of any kind, express or implied, including, but not limited to warranties of performance, merchantability and fitness for a particular purpose or product safety of any kind. Users acknowledge that in no event will the UGRC or its partners be liable to you or anyone else for any decision made or action taken in reliance on the information contained in this website, or for any indirect, consequential, special or similar damages including, without limitation, permanent loss or corruption of data, physical or emotional suffering or damages of any kind whatsoever.</li>
-              <li>Certain links in this website connect to other websites maintained by third parties over whom the UGRC and its partners have no control. These links are for general guidance on matters of interest only. The UGRC and its partners makes no representations as to the accuracy or any other aspect of information contained in other websites. The provision of these links should not be interpreted as constituting any kind of endorsement, whether implicit or explicit, of the views and/or contents of such other websites.</li>
-              <li>The user acknowledges and agrees that the UGRC and its partners reserve the right to change or revise published data and/or services on this website at any time without any legal consequence or financial liability.</li>
+              <li>The Utah Reference Network GPS (TURN GPS) is a cooperative effort involving many partners with the Utah Geospatial Resource Center (UGRC). This collaborative partnership has made it
+                possible to deliver services at a relatively low cost to users. The partners and the UGRC are committed to delivery of reliable services through TURN GPS. However, some GPS reference
+                stations are located in partnering agencies facilities that may only be accessible during regular business hours. The user understands and acknowledges that this will limit UGRC&apos;s
+                ability to immediately address problems at these facilities outside of regular business hours. Users of this service understand and acknowledge that support for connecting devices will
+                not be provided by the UGRC. Users should seek this support from their local vendors.
+              </li>
+              <li>Users of this service are responsible for verifying the accuracy of the data logged by their device for their own work. This website service is an information tool and users must
+                exercise due diligence to verify all information obtained from this site through standard surveying and geospatial practices and procedures.
+              </li>
+              <li>The corrected GPS data are provided in good faith by UGRC and its partners for its information value. Given the inherent hazards of electronic communication, users acknowledge and
+                understand that there may be omissions or inaccuracies in information contained in this website. As a result, it should not be used as a substitute for consultation with professional
+                and competent advisors. Before making any decision or taking any action based on the information contained in this site, you should consult a professional who is competent in providing
+                professional advice in your area of interest.
+              </li>
+              <li>While the UGRC and its partners have made considerable effort to ensure that the information through this service is accurate, users agree that they will not hold the UGRC and its
+                partners responsible for any errors or omissions, or for the results obtained from the use of this information. All information contained in this website is provided &quot;as is&quot;,
+                with no guarantee of completeness, accuracy, timeliness or the results obtained from the use of the information, and without warranty of any kind, express or implied, including, but
+                not limited to warranties of performance, merchantability and fitness for a particular purpose or product safety of any kind. Users acknowledge that in no event will the UGRC or its
+                partners be liable to you or anyone else for any decision made or action taken in reliance on the information contained in this website, or for any indirect, consequential, special or
+                similar damages including, without limitation, permanent loss or corruption of data, physical or emotional suffering or damages of any kind whatsoever.
+              </li>
+              <li>Certain links in this website connect to other websites maintained by third parties over whom the UGRC and its partners have no control. These links are for general guidance on
+                matters of interest only. The UGRC and its partners makes no representations as to the accuracy or any other aspect of information contained in other websites. The provision of these
+                links should not be interpreted as constituting any kind of endorsement, whether implicit or explicit, of the views and/or contents of such other websites.
+              </li>
+              <li>The user acknowledges and agrees that the UGRC and its partners reserve the right to change or revise published data and/or services on this website at any time without any legal
+                consequence or financial liability.
+              </li>
             </ol>
           </Accordion>
 
