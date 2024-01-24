4540872034e69262055bc5cc8f457819c83ed57a
Casey Wardle <chwardle@utah.gov>
2024-01-24 15:49:05 -0700

    chore: remove environment specific variable

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,8 @@ initializeApp();
 
 const vpc = params.defineString('VPC');
 const vpcEgress = 'ALL_TRAFFIC';
-const serviceAccount = 'firebase-function-v2-sa@ut-dts-agrc-turn-gps-dev.iam.gserviceaccount.com';
+const projectId = params.defineString('PROJECT_ID');
+const serviceAccount = `firebase-function-v2-sa@${projectId}.iam.gserviceaccount.com`;
 const secrets = ['secrets'];
 const corsOptions = [/ut-dts-agrc-turn-gps-dev\.firebaseapp\.com$/, /ut-dts-agrc-turn-gps-prod\.firebaseapp\.com$/, /utah\.gov/];
 
